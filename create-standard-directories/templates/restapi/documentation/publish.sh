c715bee27945f5462ad286afbe59804063938da2
Philip Callender <philcal@mac.com>
2015-03-18 15:28:32 +0800

    Updated publish.sh to pull first

--- create-standard-directories/templates/restapi/documentation/publish.sh
+++ create-standard-directories/templates/restapi/documentation/publish.sh
@@ -8,24 +8,45 @@ DOCUMENTATION_DIR=`pwd`
 echo DOCUMENTATION_DIR=${DOCUMENTATION_DIR}
 
 
-PROJECT_NAME=tea
+# Pull any updates to the github pages
+echo Pull updates from git on gh-pages branch
+echo "$" cd ${DOCUMENTATION_DIR}/site
+         cd ${DOCUMENTATION_DIR}/site
+echo "$" git pull origin gh-pages
+         git pull origin gh-pages
+if [ "$?" != "0" ] ; then
+	echo ""
+	echo "Error: Not published!"
+	exit 1
+fi
 
 # Generate stuff
-(
-	echo Generating API pages
-	cd ${DOCUMENTATION_DIR}/restapi
-	jekyll build --destination ${DOCUMENTATION_DIR}/site/restapi/
-)
+echo Generating API pages
+echo "$" cd ${DOCUMENTATION_DIR}/restapi
+         cd ${DOCUMENTATION_DIR}/restapi
+echo "$" jekyll build --destination ${DOCUMENTATION_DIR}/site/restapi/
+         jekyll build --destination ${DOCUMENTATION_DIR}/site/restapi/
+if [ "$?" != "0" ] ; then
+	echo ""
+	echo "Error: Not published!"
+	exit 1
+fi
 
 # push the github pages
-(
-	cd ${DOCUMENTATION_DIR}/site
-	echo Committing to git on gh-pages branch
-	git add .
-	git commit -m "Generated from documentation/restapi"
-	echo Pushing to Github.
-	git push origin gh-pages
-)
+echo Push updates to git on gh-pages branch
+echo "$" cd ${DOCUMENTATION_DIR}/site
+         cd ${DOCUMENTATION_DIR}/site
+echo "$" git add .
+         git add .
+echo "$" git commit -m "Generated from documentation/restapi"
+         git commit -m "Generated from documentation/restapi"
+echo "$" git push origin gh-pages
+         git push origin gh-pages
+if [ "$?" != "0" ] ; then
+	echo ""
+	echo "Error: Not published!"
+	exit 1
+fi
 
 # Display a warning
 echo "Complete"
